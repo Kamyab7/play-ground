364d61c14228a0cc11a5f307f7dedbde10e975ea
Kamyab7 <valipourkamyab.official@gmail.com>
2024-03-17 11:31:37 +0330

    api versioning with controller sample added

---
+++ api-versioning/ApiVersioningControllerSample/Controllers/V2/DummyController.cs
@@ -0,0 +1,16 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioningControllerSample.Controllers.V2;
+
+[ApiController]
+[Route("api/v{v:apiVersion}/[controller]/[action]")]
+[ApiVersion("2.0")]
+public class DummyController : ControllerBase
+{
+    [HttpGet("{name}")]
+    public IActionResult SayHello(string name)
+    {
+        return Ok($"Hello {name}");
+    }
+}
