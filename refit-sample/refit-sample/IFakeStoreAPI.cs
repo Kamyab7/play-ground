298af7ac2f0048f9d190fe52090f99fc9737268b
Kamyab7 <valipourkamyab.official@gmail.com>
2024-03-25 19:27:47 +0330

    refit sample added

---
+++ refit-sample/refit-sample/IFakeStoreAPI.cs
@@ -0,0 +1,10 @@
+﻿using Refit;
+using refit_sample.Models;
+
+namespace refit_sample;
+
+public interface IFakeStoreAPI
+{
+    [Get("/products")]
+    Task<ApiResponse<List<Product>>> GetProducts();
+}
---
+++ refit-sample/refit-sample/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace refit_sample.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public double Price { get; set; }
+    public string Description { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public string Image { get; set; } = null!;
+    public Rating Rating { get; set; } = null!;
+}
