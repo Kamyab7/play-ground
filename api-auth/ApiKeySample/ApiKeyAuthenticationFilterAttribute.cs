1ad9a28cada70e573f16cc4abdadf09e3351de25
Kamyab7 <valipourkamyab.official@gmail.com>
2024-03-30 18:14:09 +0330

    api auth sample using api key added

---
+++ api-auth/ApiKeySample/ApiKeyAuthenticationFilterAttribute.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ApiKeySample;
+
+public class ApiKeyAuthenticationFilterAttribute : ActionFilterAttribute
+{
+    private const string ApiKeyName = "X-API-KEY";
+    private const string ApiKey = "YourAPIKeyHere";
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyName, out StringValues extractedApiKey))
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+
+        if (!ApiKey.Equals(extractedApiKey))
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+
+        base.OnActionExecuting(context);
+    }
+}
+
+public class ApiKeyRequirementOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (operation.Security == null)
+            operation.Security = new List<OpenApiSecurityRequirement>();
+
+        var scheme = new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "ApiKey" }
+        };
+
+        operation.Security.Add(new OpenApiSecurityRequirement
+        {
+            [scheme] = new List<string>()
+        });
+    }
+}
