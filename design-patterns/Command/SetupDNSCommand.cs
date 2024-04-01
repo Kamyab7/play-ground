7ad904626036b24ed7f56d0464f38ed948bd9f92
Kamyab7 <valipourkamyab.official@gmail.com>
2024-04-01 16:16:20 +0330

    commands added

---
+++ design-patterns/Command/SetupDNSCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Command;
+
+public class SetupDNSCommand : ICommand
+{
+    public void Execute()
+    {
+        Task.Delay(5000).Wait();
+        Console.WriteLine("Setup DNS records");
+    }
+}
