bb9cf566b4729a6ab939aa0bf89ea4c2f7024125
Kamyab7 <valipourkamyab.official@gmail.com>
2024-04-02 18:02:56 +0330

    execution with timer sample added

---
+++ samples/ExecutionWithTimer/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Timer timer = new Timer(TimerCallback, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+
+Console.ReadLine();
+
+
+static void TimerCallback(object? state)
+{
+    // This function will be executed when the timer elapses
+    Console.WriteLine("Function executed at: " + DateTime.Now);
+    // Call your desired function here
+}
